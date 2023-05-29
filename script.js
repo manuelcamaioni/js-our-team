3468da6e33ece38a8750ac30822969994aeec5dd
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-29 15:11:39 +0200

    milestone 0

--- script.js
+++ script.js
@@ -14,10 +14,47 @@
 // Consigli del giorno:
 // Ragioniamo come sempre a step.
 // Prima la logica in italiano e poi traduciamo in codice.
-// Wayne Barnett     ounder & CEO      wayne-barnett-founder-ceo.jpg
+// Wayne Barnett     Founder & CEO      wayne-barnett-founder-ceo.jpg
 // Angela Caroll     Chief Editor      angela-caroll-chief-editor.jpg
 // Walter Gordon     Office Manager    walter-gordon-office-manager.jpg
 // Angela Lopez      Social Media      Manager  angela-lopez-social-media-manager.jpg
 // Scott Estrada     Developer         scott-estrada-developer.jpg
 // Barbara Ramos     Graphic Designer  barbara-ramos-graphic-designer.jpg
 
+const members = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        photo:'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        photo:'angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo:'walter-gordon-office-manager.jpg'
+    },
+
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media',
+        photo:'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        photo:'scott-estrada-developer.jpg'
+    },
+    
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        photo:'barbara-ramos-graphic-designer.jpg'
+    }
+];
